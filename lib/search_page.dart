9e15b69d71410cdace162ea3b60a5510ab2bedfe
oguzkarcan <oguz2001@yandex.com>
2025-04-19 21:31:09 +0300

    Improved navigation structure and bar designs

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -7,153 +7,41 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 390,
-      height: 844,
-      clipBehavior: Clip.antiAlias,
-      decoration: ShapeDecoration(
-        color: const Color(0xFFF5EFFF),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            color: const Color(0xFFE5E8E9),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Ara...',
+              hintStyle: AppFonts.entryBodyText.copyWith(
+                color: const Color(0xFF90A4AE),
+                fontSize: 14,
+              ),
+              prefixIcon: const Icon(Icons.search, color: Color(0xFF90A4AE)),
+              suffixIcon: const Icon(Icons.mic, color: Color(0xFF90A4AE)),
+              border: InputBorder.none,
+              contentPadding: const EdgeInsets.symmetric(vertical: 8),
+            ),
+          ),
         ),
       ),
-      child: Stack(
-        children: [
-          // Search Bar and Title
-          Positioned(
-            left: 16,
-            top: 29,
-            child: Container(
-              width: 331,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 48,
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: ShapeDecoration(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 24,
-                          height: 24,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: const BoxDecoration(),
-                          child: const Icon(Icons.search, color: Color(0xFF0B0B0B)),
-                        ),
-                        const SizedBox(width: 16),
-                        Text(
-                          'Search',
-                          style: AppFonts.entryTitleText.copyWith(
-                            color: const Color(0xFF0B0B0B),
-                            fontSize: 14,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Container(
-                    width: double.infinity,
-                    height: 41,
-                    padding: const EdgeInsets.all(8),
-                    decoration: ShapeDecoration(
-                      color: const Color(0xFFE5E8E9),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            height: 18,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  width: 293,
-                                  child: Text(
-                                    'Enter keywords',
-                                    style: AppFonts.entryBodyText.copyWith(
-                                      color: const Color(0xFF90A4AE),
-                                      fontSize: 12,
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: 18,
-                          height: 18,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: const BoxDecoration(),
-                          child: const Icon(Icons.mic, size: 18, color: Color(0xFF90A4AE)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          // Search Results List
-          Positioned(
-            left: 0,
-            top: 120,
-            right: 0,
-            bottom: 80,
-            child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              children: _buildSearchResults(),
-            ),
-          ),
-
-          // Bottom Navigation Bar
-          Positioned(
-            left: 41,
-            top: 780,
-            child: Container(
-              width: 307,
-              height: 64,
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(
-                    width: 1,
-                    strokeAlign: BorderSide.strokeAlignCenter,
-                  ),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildNavItem(Icons.home, false),
-                  _buildNavItem(Icons.search, true),
-                  _buildNavItem(Icons.add_circle_outline, false),
-                  _buildNavItem(Icons.person_outline, false),
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: Container(
+        color: const Color(0xFFF5EFFF),
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          children: _buildSearchResults(),
+        ),
       ),
     );
   }
@@ -209,17 +97,6 @@ class SearchPage extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildNavItem(IconData icon, bool isSelected) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      child: Icon(
-        icon,
-        color: isSelected ? AppColors.primary : Colors.grey,
-        size: 24,
       ),
     );
   }
