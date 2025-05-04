29a014cbf49db1829f4b07b816a96304575fcdf3
OkanKerem <okankeremcitak@gmail.com>
2025-05-04 16:51:34 +0300

    add env

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tatli_sozluk/utils/routes.dart'; // AppRoutes burada tanımlıysa
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
+Future<void> main() async {
   // Ensure Flutter bindings are initialized
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Initialize Firebase
-  await Firebase.initializeApp();
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+  
+  // Initialize Firebase with options from environment variables
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
+      appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+      projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    ),
+  );
   
   runApp(const MyApp());
 }
