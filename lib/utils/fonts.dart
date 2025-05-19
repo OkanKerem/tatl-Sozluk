b0be34ca56d11ee8b1938bb87b3a5bc1c9727bc4
Oğuz Karcan <oguzkarcan@192.168.1.103>
2025-05-19 17:41:20 +0300

    Enhanced color system and typography

--- lib/utils/fonts.dart
+++ lib/utils/fonts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppFonts {
+  // User interface elements
   static const TextStyle usernameText = TextStyle(
     color: Colors.black,
     fontSize: 20,
@@ -19,6 +20,7 @@ class AppFonts {
     letterSpacing: -0.22,
   );
 
+  // Counter elements
   static const TextStyle countText = TextStyle(
     color: Colors.black,
     fontSize: 25,
@@ -28,6 +30,7 @@ class AppFonts {
     letterSpacing: -0.28,
   );
 
+  // Entry related text styles
   static const TextStyle entriesLabelText = TextStyle(
     color: Colors.black,
     fontSize: 25,
@@ -55,6 +58,25 @@ class AppFonts {
     letterSpacing: -0.18,
   );
 
+  // Button text styles
+  static const TextStyle primaryButtonText = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontFamily: 'Itim',
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+    letterSpacing: -0.2,
+  );
+  
+  static const TextStyle secondaryButtonText = TextStyle(
+    color: Colors.black87,
+    fontSize: 18,
+    fontFamily: 'Itim',
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.2,
+  );
+
   static const TextStyle deleteButtonText = TextStyle(
     color: Color(0xFFFDF1F1),
     fontSize: 20,
@@ -62,5 +84,24 @@ class AppFonts {
     fontWeight: FontWeight.w400,
     height: 1.5,
     letterSpacing: -0.22,
+  );
+  
+  // Form text styles
+  static const TextStyle inputLabelText = TextStyle(
+    color: Colors.black54,
+    fontSize: 16,
+    fontFamily: 'Itim',
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.18,
+  );
+  
+  static const TextStyle inputText = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Itim',
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: -0.2,
   );
 }
